77bac9f4bc69af7b27a9f3f63591bdcd3a1659d1
exploring-curiosity <curiositywake@gmail.com>
2021-05-27 21:34:14 +0530

    Single Player done

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,5 @@
 import {useCallback, useState} from 'react';
-import { STAGE_WIDTH } from '../gameHelper';
+import { checkCollision, STAGE_WIDTH } from '../gameHelper';
 
 import { TETROMINOS,randomTetromino } from '../tetrominos';
 
@@ -9,6 +9,31 @@ export const usePlayer = () =>{
         tetromino: TETROMINOS[0].shape,
         collided:false
     });
+
+    const rotate=(matrix,dir)=>{
+        const rotateTetro = matrix.map((_,index)=>matrix.map(col=>col[index]))
+        if(dir>0) return rotateTetro.map(row=>row.reverse());
+        return rotateTetro.reverse();
+    }
+
+    const playerRotate = (stage,dir)=>{
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino,dir);
+
+        const pos = clonedPlayer.pos.x;
+        let offset=1;
+        while(checkCollision(clonedPlayer,stage,{x:0,y:0})){
+            clonedPlayer.pos.x+=offset;
+            offset=-(offset+(offset>0?1:-1));
+            if(offset>clonedPlayer.tetromino[0].length){
+                rotate(clonedPlayer.tetromino,-dir);
+                clonedPlayer.pos.x=pos;
+                return;
+            }
+        }
+
+        setPlayer(clonedPlayer);
+    }
 
     const updatePlayerPos = ({x,y,collided})=>{
         setPlayer(prev=>({
@@ -26,5 +51,5 @@ export const usePlayer = () =>{
         })
     },[])
 
-    return [player,updatePlayerPos,resetPlayer];
+    return [player,updatePlayerPos,resetPlayer,playerRotate];
 }
